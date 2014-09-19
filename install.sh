f85f6073898ac7fcf5e77761a63f53ab4d74082f
Patrick Paskaris <patrick@paskaris.gr>
2014-09-18 21:46:14 -0400

    add bashrc, inputrc and install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# symlink the dotfiles \o/
+dotfiles="vimrc gitconfig aliases bashrc3p0 inputrc"
+for dotfile in $dotfiles ; do
+	ln -nsf ~/dotfiles/$dotfile ~/.$dotfile
+done
+
+# get the git-prompt.sh
+curl --silent https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+# put the bashrc3p0 in the bashrc
+if ! grep -Fxq "bashrc3p0" ~/.bashrc ; then
+	echo ". ~/.bashrc3p0" >> ~/.bashrc
+fi
+
+# setup the vundles
+vundir="$HOME/.vim/bundle/Vundle.vim"
+if command -v git >/dev/null 2>&1 ; then
+	if [ -d $vundir ] ; then
+		git -C $vundir pull
+	else
+		git clone https://github.com/gmarik/Vundle.vim.git $vundir
+	fi
+else
+	echo "skipping vundle plugins because git is missing"
+fi
+
+if command -v vim >/dev/null 2>&1 ; then
+	vim +PluginInstall +qall
+else
+	echo "skipping vundle install because vim is missing"
+fi
+
+# setup the nodes
+modules="jshint"
+if command -v npm >/dev/null 2>&1 ; then
+	mkdir -p ~/.node
+	npm install --prefix ~/.node $modules
+else
+	echo "skipping npm packages because npm is missing"
+fi

9d2192069115bfe916ba42471a0cbf407ef0093f
Patrick Paskaris <ppaskaris@coursera.org>
2022-10-17 14:48:20 -0400

    More tweaks

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-set -x
+set -ex
 
-dotfiles="vimrc gitconfig zshrc3p0"
+dotfiles="vimrc gitconfig gitexcludes zshrc3p0"
 for dotfile in $dotfiles ; do
 	ln -nsf ~/dotfiles/$dotfile ~/.$dotfile
 done
@@ -9,25 +9,14 @@ mkdir -p ~/.bin
 mkdir -p ~/.vim-backup
 mkdir -p ~/.vim-swap
 
-curl --silent https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+GitPromptPath="$HOME/.zsh/git-prompt"
+[[ -d $GitPromptPath ]] && git -C $GitPromptPath pull || git clone git@github.com:olivierverdier/zsh-git-prompt.git $GitPromptPath
 
 if ! grep -Fxq "source ~/.zshrc3p0" ~/.zshrc ; then
 	echo "source ~/.zshrc3p0" >> ~/.zshrc
 fi
 
-vundir="$HOME/.vim/bundle/Vundle.vim"
-if command -v git >/dev/null 2>&1 ; then
-	if [ -d $vundir ] ; then
-		git -C $vundir pull
-	else
-		git clone https://github.com/gmarik/Vundle.vim.git $vundir
-	fi
-else
-	echo "skipping vundle plugins because git is missing"
-fi
+VundlePath="$HOME/.vim/bundle/Vundle.vim"
+[[ -d $VundlePath ]] && git -C $VundlePath pull || git clone https://github.com/gmarik/Vundle.vim.git $VundlePath
 
-if command -v vim >/dev/null 2>&1 ; then
-	vim +PluginInstall +qall
-else
-	echo "skipping vundle install because vim is missing"
-fi
+vim +PluginInstall +qall
